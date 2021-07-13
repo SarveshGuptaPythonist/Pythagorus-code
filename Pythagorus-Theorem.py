ccbac4583822c6fba8f51ea856933ba6076cd0fb
sarveshguptapythonist <sarveshgupta67890@gmail.com>
2021-07-14 00:10:15 +0530

    TC and SC added

--- Pythagorus-Theorem.py
+++ Pythagorus-Theorem.py
@@ -2,20 +2,24 @@ class Number:
     MAX_SAFE_INTEGER = 9007199254740990
 
 num = Number().MAX_SAFE_INTEGER
-powerof2 = [0]
-for i in range(1, num + 1):
-    powerof2.append(i**2)
+powerof2 = [0]                                                 # O(1)
+for i in range(1, num + 1):                                    # O(N)
+    powerof2.append(i**2)                                      # O(log(i)**2)
 
-mapper = {}
-for i in range(1, num):
-    for j in range(1, num):
+mapper = {}                                                    # O(1)
+for i in range(1, num):                                        # }
+    for j in range(1, num):                                    # }O(N^2)
         a = i if i>j else j
         b = i if i<j else j
         if (a, b) not in mapper:
             mapper[(a, b)] = powerof2[i] + powerof2[j]
 
 
-for k in range(1, num):
-    for val in mapper:
+for k in range(1, num):                                       # }
+    for val in mapper:                                        # }O(N^2)
         if mapper[val] == powerof2[k]:
             print(val[0], val[1], k)
+            
+
+# Time  Complexity = O(N^2)
+# Space Complexity = O(N^2)
